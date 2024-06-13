48d060f1de46d4476c5403e2a4209f53957291dc
Harsh choudhary <pyash4048@gmail.com>
2024-06-13 12:06:48 +0530

    created frontend part of main display

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -4,7 +4,7 @@ import DisplayHome from './DisplayHome'
 
 const Display = () => {
   return (
-    <div className='w-[100%] h-[98%] px-6 py-4 rounded m-2 bg-[#121212] h-full'>
+    <div className='w-[100%] h-[98%] px-6 py-4 overflow-auto rounded m-2 bg-[#121212] h-full'>
       <Routes>
         <Route path='/' element={<DisplayHome/>} />
       </Routes>
--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Navbar from './Navbar'
+import {albumsData,songsData} from '../assets/assets'
+import AlbumItem from './AlbumItem'
+import SongsItem from './SongsItem'
 
 const DisplayHome = () => {
   return (
     <>
         <Navbar />
+        <h1 className='text-white my-5 text-2xl font-bold'>Featured Chart's</h1>
+        <div className='my-4 flex overflow-auto gap-5 '>
+            {albumsData.map((items,index)=>(<AlbumItem key={index} image={items.image} name={items.name} desc={items.desc} id={items.id} />))}
+        </div>
+        <h1 className='text-white my-5 text-2xl font-bold'>Today's biggest hits</h1>
+        <div className='my-4 flex overflow-x-auto gap-5 '>
+            {songsData.map((items,index)=>(<SongsItem key={index} image={items.image} name={items.name} desc={items.desc} id={items.id} />))}
+        </div>
     </>
   )
 }
